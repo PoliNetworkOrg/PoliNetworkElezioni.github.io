70f954a083ddd1c465e6339f478e76ff598cc9b6
PoliNetworkElezioni <PoliNetworkElezioni@users.noreply.github.com>
2021-05-05 11:43:43 +0000

    deploy: ae49edd3e78abbe209ab296f70e9da8828bd44dc

---
+++ _nuxt/static/1620215018/lista/studentiindipendenti/payload.js
@@ -0,0 +1,1 @@
+__NUXT_JSONP__("/lista/studentiindipendenti", {data:[{}],fetch:{},mutations:void 0});
---
+++ js/custom.js
@@ -0,0 +1,13 @@
+document.addEventListener("DOMContentLoaded", function(){
+    for (var j=0; j < 5; j++)
+    {
+        var ul =  document.getElementById("listeContainer");
+        if (ul != null)
+        {
+            for (var i = ul.children.length; i >= 0; i--) {
+                ul.appendChild(ul.children[Math.random() * i | 0]);
+            }
+        }
+    }
+});
+
