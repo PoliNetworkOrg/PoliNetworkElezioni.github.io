2700e82274edbbc053574b6390abf7a91e741eb7
Federico Armellini <armellini.federico@gmail.com>
2021-05-05 22:25:59 +0200

    senato cda lista aperta

--- pages/scuola/auic.vue
+++ pages/scuola/auic.vue
@@ -48,13 +48,22 @@ export default {
                     {
                         name: "AUIC",
                         desc: "La nostra idea di scuola auic ecc...",
-           	            candidati:[
-                            {id:1, imgUrl: "/img/candidati/listaaperta/elena_picchetti.jpg", name: "Picchetti Elena", detto: ""},
-                            {id:2, imgUrl: "/img/candidati/listaaperta/davide_diceglie.jpg", name: "Di Ceglie Davide", detto: "REFLASH"},
-                            {id:3, imgUrl: "/img/candidati/listaaperta/ian_fernandez.jpg", name: "Fernàndez Nava Ian Anthony Alloxis", detto: "IANF"},
-                            {id:4, imgUrl: "/img/candidati/listaaperta/giacomo_schiavone.jpg", name: "Schiavone Giacomo", detto: "GIACK"},
-                            {id:5, imgUrl: "/img/candidati/listaaperta/dalila_melotti.jpg", name: "Melotti Dalila", detto: "DIDI"}
-					    ]
+           	            candidati:
+                        [
+                            {id:1, imgUrl: "/img/candidati/listaaperta/tommaso_sanese.jpg", name: "Sanese Tommaso", detto: "TOMMI"},
+                            {id:2, imgUrl: "/img/candidati/listaaperta/maria_zini.jpg", name: "Zini Maria", detto: "MERI"},
+                            {id:3, imgUrl: "/img/candidati/listaaperta/sara_galli.jpg", name: "Galli Sara", detto: "SARA"},
+                            {id:4, imgUrl: "/img/candidati/listaaperta/issa_tallawi.jpg", name: "Tallawi Issa", detto: "ISSA"},
+                            {id:5, imgUrl: "/img/candidati/listaaperta/veronica_cantale.jpg", name: "Cantale Maria Veronica", detto: ""},
+                            {id:6, imgUrl: "/img/candidati/listaaperta/elisabetta_alberton.jpg", name: "Alberton Elisabetta", detto: "BETTA"},
+                            {id:7, imgUrl: "/img/candidati/listaaperta/elisabetta_taffa.jpg", name: "Taffa Elisabetta", detto: "ELI"},
+                            {id:8, imgUrl: "/img/candidati/listaaperta/marco_galbiati.jpg", name: "Galbiati Marco", detto: "CALI"},
+                            {id:9, imgUrl: "/img/candidati/listaaperta/marta_defre.jpg", name: "De fre Marta", detto: "MARTA"},
+                            {id:10, imgUrl: "/img/candidati/listaaperta/pietro_rabaioli.jpg", name: "Rabaioli Pietro", detto: "RABA"},
+                            {id:11, imgUrl: "/img/candidati/listaaperta/isaac_young.jpg", name: "Young Isaac Jason", detto: "ISAAC"},
+                            {id:12, imgUrl: "/img/candidati/listaaperta/marco_stringhetti.jpg", name: "Stringhetti Marco", detto: "STRINGO"},
+                            {id:13, imgUrl: "/img/candidati/listaaperta/sara_baldelli.jpg", name: "Baldelli Sara Micol", detto: ""}
+                        ]
                     }
                 ]
             }
