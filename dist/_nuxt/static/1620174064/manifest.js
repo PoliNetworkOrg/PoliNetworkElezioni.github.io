3f062618aa4e0d611e684ae6803a848f96fa65d5
Federico Armellini <armellini.federico@gmail.com>
2021-05-05 02:23:00 +0200

    generate

---
+++ dist/_nuxt/static/1620174064/manifest.js
@@ -0,0 +1,1 @@
+__NUXT_JSONP__("manifest.js", {routes:["\u002F","\u002Fscuole","\u002Fccs","\u002Flista\u002Flistaaperta","\u002Fcontatti","\u002Fistruzioni","\u002Flista\u002Fstudentiindipendenti","\u002Fsenatocda","\u002Flista\u002Fternasinistrorsa","\u002Flista\u002Fsvoltastudenti"]})
