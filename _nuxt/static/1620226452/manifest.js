508a65da3c26b8fda23cdc4d4c8c3870a4ab9692
ArmeF97 <ArmeF97@users.noreply.github.com>
2021-05-05 14:54:18 +0000

    deploy: 9e6820768028e21c05c6f8632670a1f9f13eb239

---
+++ _nuxt/static/1620226452/manifest.js
@@ -0,0 +1,1 @@
+__NUXT_JSONP__("manifest.js", {routes:["\u002F","\u002Fccs","\u002Fscuole","\u002Flista\u002Flistaaperta","\u002Fcontatti","\u002Fistruzioni","\u002Fsenatocda","\u002Flista\u002Fternasinistrorsa","\u002Flista\u002Fsvoltastudenti","\u002Flista\u002Fstudentiindipendenti"]})
