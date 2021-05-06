63406bc29b5026915baa06c9a5aa21881baf8782
Eliaxie <elia.maggioni@gmail.com>
2021-05-06 22:23:22 +0200

    fix navbar

--- components/NavigationBar.vue
+++ components/NavigationBar.vue
@@ -1,7 +1,7 @@
 <template>
   <!-- Navigation -->
   <nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
-    <div class="container">
+    <div class="containerNavBar">
 		<a class="navbar-brand" href="/"><img src="/img/icon.png" style="width:calc(20px + 0.1rem);"><span>&nbsp;PoliNetwork</span></a>
 		<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
 			<span class="navbar-toggler-icon"></span>
