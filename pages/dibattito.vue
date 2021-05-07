93d13c1f8ae24c2c75ac31dc8b508e2e992876d8
Federico Armellini <armellini.federico@gmail.com>
2021-05-07 16:23:40 +0200

    mesi maiuscolo in dibattito

--- pages/dibattito.vue
+++ pages/dibattito.vue
@@ -5,7 +5,7 @@
 
 <div style="padding:calc(6px + 1.5rem);"></div>
 	 <div class="container3 lead" style="text-align:center;padding-top:calc(10px + 1rem);padding-bottom:calc(0.5rem);">
-		<h3 class="dibattito">🗣 Dibattito 13, 14, 15 Maggio 2021</h3>
+		<h3 class="dibattito">🗣 Dibattito 13-14-15 Maggio 2021</h3>
         <div class="container9">
 		<span class="container3" style="padding-top:3px;">
 						Le liste di rappresentanza si affronteranno in una serie di dibattiti tematici in presenza, organizzati da <a href="https://www.poliradio.it/">POLI.RADIO</a>.
@@ -23,10 +23,10 @@
 <div class="container5">
 	<div  style="width:100%;text-align:center;display:block;" class="container2">
 	<br />
-	<h3 class="dibattito">Dibattito 28 ottobre 2020</h3>
+	<h3 class="dibattito">Dibattito 28 Ottobre 2020</h3>
 		<div class="container8">
 		<span class="lead" >
-		In attesa del nuovo dibattito che organizzeremo fra liste (previsto il 13,14,15 maggio) potete guardare quello svoltosi ad ottobre 2020
+		In attesa del nuovo dibattito che organizzeremo fra liste (previsto il 13-14-15 Maggio) potete guardare quello svoltosi ad Ottobre 2020
 		</span>
 		</div>
 	</div>
