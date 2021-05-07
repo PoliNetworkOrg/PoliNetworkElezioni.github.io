2c4a7746f8f5d0054260ea1b0ec82ca5cf95e824
Federico Armellini <armellini.federico@gmail.com>
2021-05-07 17:41:49 +0200

    stile calendario

--- components/Calendario.vue
+++ components/Calendario.vue
@@ -4,36 +4,55 @@
 <div class="lead"
 		 style="padding:calc(5px + 0.1rem);border: 1px solid #0000002b;border-radius: calc(10px + 0.2rem);max-width:calc(9rem + 9vw + 150px);text-align:center;margin:auto;"
 		 >
-			<table style="width:100%;margin:auto;">
-				<tr>
-					<td style="padding-right:0.5rem;">
-						🗓
+		 	<div style="padding-left:5px;padding-right:5px;">
+				<table style="width:100%;margin:auto;padding-left:0px;padding-right:0px;">
+					<tr>
+						<td style="padding-right:0.1rem;text-align:center;" >
+							🗓
 						</td>
 						<td>
 							<table style="text-align:center;width:100%;">
 								<tr>
-		<td style="padding-left:calc(6px + 0.1rem);padding-right:calc(6px + 0.1rem);">
-Lunedì 24/05/2021
-										</td>
-									<td style="padding-left:calc(6px + 0.1rem);padding-right:calc(6px + 0.1rem);">
-											Giovedì 27/05/2021
-											</td>
+									<td class="calendarioTD" style="padding-left:calc(6px + 0.1rem);padding-right:calc(6px + 0.1rem);">
+										Lunedì 24/05/2021
+									</td>
 								</tr>
-														<tr>
-						<td style="padding-left:calc(6px + 0.1rem);padding-right:calc(6px + 0.1rem);">
-ore 09.00
-										</td>
-						<td style="padding-left:calc(6px + 0.1rem);padding-right:calc(6px + 0.1rem);">
-									ore 23.59
-											</td>
+								<tr>
+									<td class="calendarioTD"  style="padding-left:calc(6px + 0.1rem);padding-right:calc(6px + 0.1rem);">
+										ore 09.00
+									</td>
 								</tr>
-								
-								</table>
-							</td>
+							</table>
+						</td>
+						<td>
+							-
+						</td>
+						<td>
+							<table style="text-align:center;width:100%;">
+								<tr>
+									<td class="calendarioTD"  style="padding-left:calc(6px + 0.1rem);padding-right:calc(6px + 0.1rem);">
+										Giovedì 27/05/2021
+									</td>
+								</tr>
+								<tr>
+									<td  class="calendarioTD" style="padding-left:calc(6px + 0.1rem);padding-right:calc(6px + 0.1rem);">
+										ore 23.59
+									</td>
+								</tr>
+							</table>
+							
+						</td>
 					</tr>
-			 
-			</table>
+
+				</table>
+			</div>
 		</div>
 
         </div>
 </template>
+
+<style >
+.calendarioTD{
+	font-size: calc(1.1rem + 0.12vw);
+}
+</style>
