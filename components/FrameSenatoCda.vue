f3495fd8d9b3e6afb6ae60afff9eec2040848015
Federico Armellini <armellini.federico@gmail.com>
2021-05-07 11:03:47 +0200

    stile

--- components/FrameSenatoCda.vue
+++ components/FrameSenatoCda.vue
@@ -4,7 +4,7 @@
         <div style="width:100%;">
             <h2>{{item.name}}</h2>
         </div>
-        <div style="padding-bottom:calc(0.3rem + 5px);">
+        <div class="descrizioneFrameSenatoCdA">
             <span style="font-size:calc(1px + 1rem);color:black;padding:calc(1px + 1vw);">
                 {{item.desc}}
             </span>
@@ -39,3 +39,14 @@ export default {
     }
 }
 </script>
+
+<style >
+.descrizioneFrameSenatoCdA{
+    margin:auto;
+    width: 100%;
+    max-width: calc(70px + 15rem + 40vw);
+    padding-left:calc(1vw + 1px + 0.07rem);
+    padding-right:calc(1vw + 1px + 0.07rem);
+    padding-bottom:calc(0.3rem + 6px);
+}
+</style>
