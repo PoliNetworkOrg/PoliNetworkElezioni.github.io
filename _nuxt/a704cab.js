21c857c64fa71d61565cb228086add6d90865f65
ArmeF97 <ArmeF97@users.noreply.github.com>
2021-05-09 11:50:30 +0000

    deploy: 23f9ab82d9b796fcbdaa5eb62af684d3c7f0f861

--- _nuxt/a704cab.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[40],{284:function(e,n,t){"use strict";t.r(n);var r={middleware:"redirect"},l=t(34),component=Object(l.a)(r,(function(){var e=this.$createElement;return(this._self._c||e)("div")}),[],!1,null,null,null);n.default=component.exports}}]);
