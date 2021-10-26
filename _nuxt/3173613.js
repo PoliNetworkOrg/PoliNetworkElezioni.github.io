1afcb7973a9527aaa74a1559fea6e7bebdf29824
ArmeF97 <ArmeF97@users.noreply.github.com>
2021-10-26 08:34:53 +0000

    deploy: 311cbea7272e5a3688a5c35b043a87a24f9a3db0

---
+++ _nuxt/3173613.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[28],{319:function(n,e,t){"use strict";t.r(e);var l=t(22),component=Object(l.a)({},(function(){var n=this.$createElement,e=this._self._c||n;return e("div",[e("PagesMain",{attrs:{language:"en"}})],1)}),[],!1,null,null,null);e.default=component.exports;installComponents(component,{PagesMain:t(275).default})}}]);
