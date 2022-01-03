923ccd13d31cbbd525ad2bad4cc8ebdace356387
steven9463912 <steven9463912@gmail.com>
2022-01-03 17:29:04 +0100

    Update index.vue and organi.vue

--- pages/organi.vue
+++ pages/organi.vue
@@ -1,29 +1,23 @@
 <template>
-    <div>
+  <div>
+    <LayoutNavigationBar :language="$i18n.locale" />
 
-        <LayoutNavigationBar :language="$i18n.locale" />
-
-        <!-- Page Content -->
-        <div class="container2">
-
-            <!-- Jumbotron Header -->
-            <header class="jumbotron my-4 testoCentrale" style="border:none;">
-                <div style="text-align:center;">
-                    <h2>Organi</h2>
-             
-                
-                
-                </div>
-            </header>
-
-            <SubpagesOrgani  :language="$i18n.locale" />
+    <!-- Page Content -->
+    <div class="container2">
+      <!-- Jumbotron Header -->
+      <header class="jumbotron my-4 testoCentrale" style="border: none">
+        <div style="text-align: center">
+          <h2>Organi</h2>
         </div>
-        <!-- /.container -->
+      </header>
 
-        <div style="padding:calc(50px + 1vw + 1vh);">
-        </div>
-
-        <LayoutFooter :language="$i18n.locale" />
+      <SubpagesOrgani :language="$i18n.locale" />
     </div>
+    <!-- /.container -->
+
+    <div style="padding: calc(50px + 1vw + 1vh)"></div>
+
+    <LayoutFooter :language="$i18n.locale" />
+  </div>
 </template>
 
