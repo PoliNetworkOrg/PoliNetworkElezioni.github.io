cf531f0b0b8cf7c895ee5161818a41b01c1562f9
steven9463912 <steven9463912@users.noreply.github.com>
2022-07-12 10:09:27 +0000

    deploy: 07c4a5e7caa228f1b465cc9eef7828281c74077f

--- _nuxt/static/1657620223/2021/scuola/design/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1657620223",layout:"default",error:a,serverRendered:true,routePath:"\u002F2021\u002Fscuola\u002Fdesign",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a}}}}(null));
